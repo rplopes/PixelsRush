e0bb3a34cfe5e4872f81e4723e9b8384ede26279
Erbarbar <filipelopez3@gmail.com>
2016-10-06 22:07:54 +0100

    Simplify project to get a basic level

---
+++ Assets/gameScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameScript : MonoBehaviour {
+
+    public GameObject obstaclePrefab;
+    [Range(-.01f,.01f)]
+    public float obstacleSpeedY;
+
+    private GameObject obstacles;
+
+    // Use this for initialization
+    void Start () {
+        obstacles =  (GameObject)Instantiate(obstaclePrefab, new Vector3(0f, 5f, 5f), Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        obstacles.transform.position = new Vector3( obstacles.transform.position.x, 
+                                                    obstacles.transform.position.y - obstacleSpeedY, 
+                                                    obstacles.transform.position.z);
+	}
+}
