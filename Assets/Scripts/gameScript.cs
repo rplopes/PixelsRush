27a0290aa1212e6685ee2832c4ea97987189d469
Ricardo Lopes <mail@ricardolopes.net>
2016-10-07 08:47:59 +0100

    Add first obstacles

--- Assets/Scripts/gameScript.cs
+++ Assets/Scripts/gameScript.cs
@@ -4,21 +4,36 @@ using System.Collections;
 public class gameScript : MonoBehaviour {
 	public static int nCircles = 10;
 	public static int circleDistance = 8;
+	public static float spawnRate = 0.9f;
+
+	private static int spawnDistance = gameScript.nCircles * gameScript.circleDistance;
 
 	public GameObject circleLimitPrefab;
+	public GameObject deathStarPrefab;
+	public GameObject bowserPrefab;
 	public float speed;
 
 	private GameObject[] circles = new GameObject[nCircles];
 
     // Use this for initialization
     void Start () {
+		Invoke("SpawnObstacle", spawnRate);
+
 		// Instantiate circle limits
 		for (int i = 0; i < nCircles; i++) {
 			circles[i] = ((GameObject) Instantiate(circleLimitPrefab, new Vector3(0f, 0f, (float) (i * circleDistance)), Quaternion.identity));
 		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void SpawnObstacle () {
+		double r = Random.value;
+
+		if (r < 0.5) {
+			Instantiate (deathStarPrefab, new Vector3 (Random.insideUnitCircle.x * 2.2f, Random.insideUnitCircle.y * 2.2f, spawnDistance), Quaternion.identity);
+		} else {
+			Instantiate (bowserPrefab, new Vector3 (Random.insideUnitCircle.x * 2f, Random.insideUnitCircle.y * 2f, spawnDistance), Quaternion.identity);
+		}
+
+		Invoke("SpawnObstacle", spawnRate);
 	}
 }
---
+++ Assets/Scripts/obstacleScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class obstacleScript : MonoBehaviour {
+
+	void FixedUpdate () {
+		float newZ = gameObject.transform.position.z - GameObject.Find ("Game Master").GetComponent<gameScript> ().speed;
+
+		if (newZ < -1) {
+			Destroy (gameObject);
+
+			return;
+		}
+
+		gameObject.transform.position = new Vector3( gameObject.transform.position.x, 
+			gameObject.transform.position.y, 
+			newZ);
+	}
+}
